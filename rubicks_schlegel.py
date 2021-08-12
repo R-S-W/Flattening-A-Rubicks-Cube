c4d30d417d43e1e41feb1292115307e86433d890
Ray <raysidarta@gmail.com>
2021-08-12 00:14:44 -0400

    Change colors for animations

--- rubicks_schlegel.py
+++ rubicks_schlegel.py
@@ -5,15 +5,21 @@ import sys
 if len(sys.argv)<4: print('Please add the diagram number')
 
 diagramNumber = int(sys.argv[3])
-cameraSettingNumber = 3
+cameraSettingNumber = 4
 if len(sys.argv) >4:  cameraSettingNumber = int(sys.argv[4])
 #1 = Clover, 2 = 2-Pair, 3 = Tri-Pair
 
 #_____Entity Constants and Parameters_____
 alpha = 5/6*PI/2
 ca = np.cos(alpha)
+RED2= "#BF0000"
+GREEN2 = "#038800"
+BLUE2 = "#3240AF"
+
 colorArr1 = [WHITE, WHITE, BLUE, GREEN, ORANGE, RED]
-colorArrB2 = [RED, RED, BLUE, BLUE, GREEN, GREEN]
+colorArrB2 = [RED2, "#660000", BLUE_E, BLUE_E, "#005F20", GREEN2]
+colorArrB3 = [RED2, RED2, BLUE2,BLUE2, GREEN2, GREEN2]
+colorArrB3_2 = [RED2, "#660000", "#1A278A",BLUE2, "#005F20", GREEN2]
 colorArrA2 = [colorArrB2[1], colorArrB2[0], colorArrB2[2],colorArrB2[3], 
 	colorArrB2[4], colorArrB2[5]]
 
@@ -22,7 +28,7 @@ colorArrA2 = [colorArrB2[1], colorArrB2[0], colorArrB2[2],colorArrB2[3],
 #---Create Rubicks Sphere  (Shape A)---
 if diagramNumber == 1: aColors = colorArr1
 elif diagramNumber == 2: aColors = colorArrA2
-else: aColors = colorArrB2
+else: aColors = colorArrB3
 farrA = [Circle(color=x) for x in aColors]
 
 '''After transformations, farrA has circles 1 and 2 perpendicular to z,
@@ -90,7 +96,7 @@ elif diagramNumber==3:
 		x.rotate_about_origin(45*DEGREES, axis=UP)
 		x.rotate_about_origin(-45*DEGREES,axis = RIGHT)
 
-	farrB = [Circle(color =x) for x in colorArrB2]
+	farrB = [Circle(color =x) for x in colorArrB3]
 	smR = 2/3.
 	l = .7*2/3
 	farrB[3].shift((0,-l,0))
@@ -130,7 +136,7 @@ class RubicksSchlegelTransform(Scene):
 		axes = ThreeDAxes()#**axis_config)
 		self.add(axes) 
 		frame = self.camera.frame
-		if cameraSettingNumber == 1 or cameraSettingNumber==2:
+		if cameraSettingNumber <4:
 			frame.set_euler_angles(
 					theta=30 * DEGREES,
 					phi=75 * DEGREES,
@@ -139,11 +145,14 @@ class RubicksSchlegelTransform(Scene):
 		#_____Create Entities_____
 		text2d = Text("Hello")
 		text2d.to_edge(UP+RIGHT)
+		# initSphere = Sphere(color = GREY)
+		# initSphere.scale(.8*np.sin(alpha))
 
 
 		
 		#_____Add Shapes_____
 		self.add(text2d)
+		# self.add(initSphere)
 		for x in farrA: self.add(x)
 		
 		#_____Animate_____
@@ -151,12 +160,15 @@ class RubicksSchlegelTransform(Scene):
 		if cameraSettingNumber == 1:
 			self.play(frame.animate.increment_theta(-55*DEGREES),run_time = 2	)
 			self.play(frame.animate.increment_phi(-25*DEGREES),run_time = 2	)
-			self.wait(1)
 		if cameraSettingNumber == 2:
+			self.play(frame.animate.increment_theta(-40*DEGREES), run_time=2	)
+			self.play(frame.animate.increment_phi(-50*DEGREES), run_time=2	)
+		if cameraSettingNumber == 3:
 			self.play(frame.animate.increment_theta(-55*DEGREES), run_time=2	)
 			self.play(frame.animate.increment_phi(-50*DEGREES), run_time=2	)
-			self.wait(1)
+		self.wait(1)
 
+		# a_to_b.append(FadeOut(initSphere))
 		self.play(*a_to_b, run_time = 4)
 		self.wait(4)
 
